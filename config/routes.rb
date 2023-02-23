229f2b61b0588f0943499336b3689c494db7ec2c
Eric Le Borgne <eric.dev91@gmail.com>
2023-02-23 16:51:45 +0100

    step 3 exo ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :lists, only: %i[index show new create] do
+    resources :bookmarks, only: %i[new create]
+    # Defines the root path route ("/")
+    # root "articles#index"
+  end
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :bookmarks, only: [:destroy]
 end
